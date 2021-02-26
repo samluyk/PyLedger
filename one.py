3124d961c0c6a5189541b252e94a5887d87130a1
Sam Luyk <11977644+samluyk@users.noreply.github.com>
2021-02-25 20:51:07 -0500

    Update one.py

--- one.py
+++ one.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,FloatField,DateField,validators
 
 app = Flask(__name__)
 
-@app.route('/') #127.0.0.1:5000
+app.config['SECRET_KEY'] = 'temporarysecuritykey'
+
+# Below is the fields the form will have
+class InfoForm(FlaskForm):
+
+    breed = StringField("What breed are ya?? ")
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    breed = False
+    form = InfoForm()
+    if form.validate_on_submit():
+        breed = form.breed.data
+        form.breed.data = ''
+    return render_template('index.html',form=form,breed=breed)
 
 if __name__ == '__main__':
         app.run(debug=True)
