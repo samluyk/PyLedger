7d6ba343ed6a7eba7df39740a26115e5180f1f47
sam <samluyk@protonmail.com>
2021-02-26 19:50:38 -0500

    Basic form creation

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This program was created to be a digital ledger that allows me to store and cate
 This is meant only to run locally to help compensate for my lack of proper code/best practice knowledge. You should do the same.
 
 The idea for this was based off of this linked google sheet/form. I was hoping for similar functionality, just fully self-hostable and private. https://www.reddit.com/r/personalfinance/comments/53ktyv/budget_spreadsheet_with_google_form_for_tracking/
+
+This code was possible thanks to this udemy course:
+https://www.udemy.com/course/python-and-flask-bootcamp-create-websites-using-flask/learn/lecture/10533538#overview
--- one.py
+++ one.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session,redirect,url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,FloatField,DateField,validators
+from wtforms import StringField,SubmitField,FloatField,DateField,validators,TextField,SelectField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'temporarysecuritykey'
 
 # Below is the fields the form will have
-class InfoForm(FlaskForm):
+class ExpenseForm(FlaskForm):
 
-    breed = StringField("What breed are ya?? ")
+    theExpense = StringField("Enter your expense: ",validators=[DataRequired()])
+    expenseAmount = FloatField("Enter the amount: ",validators=[DataRequired()])
+    #expensePrimaryCategory = StringField("Choose the primary category: ",validators=[DataRequired()])
+    #expenseSecondaryCategory = StringField("Choose the secondary category: ",validators=[DataRequired()])
+    expenseAbout = TextField("Enter any comments: ")
     submit = SubmitField('Submit')
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    breed = False
-    form = InfoForm()
+    #theExpense = False
+    form = ExpenseForm()
     if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ''
-    return render_template('index.html',form=form,breed=breed)
+        session['theExpense'] = form.theExpense.data
+        session['expenseAmount'] = form.expenseAmount.data
+        session['expenseAbout'] = form.expenseAbout.data
+        #theExpense = form.theExpense.data
+        #form.theExpense.data = ''
+        return redirect(url_for('submitted'))
+
+    return render_template('index.html',form=form)
+    
+@app.route("/submitted")
+def submitted():
+    return render_template('submitted.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
